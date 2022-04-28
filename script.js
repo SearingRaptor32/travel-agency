edfdc35b90882b678389e3395042643ca66d76ee
BuildTools <billyskaramanolis@outlook.com>
2022-04-28 03:27:27 +0300

    initialcommit

--- script.js
+++ script.js
@@ -13,3 +13,29 @@ function downvote() {
   if (vote_n>0) {vote_n = vote_n - 1}
   document.getElementById("likes").innerHTML = vote_n + " likes!"
 }
+// Get the elements with class="column"
+var elements = document.getElementsByClassName("column");
+
+// Declare a "loop" variable
+var i;
+
+// Full-width images
+function one() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.flex = "100%";
+  }
+}
+
+// Two images side by side
+function two() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.flex = "50%";
+  }
+}
+
+// Four images side by side
+function four() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.flex = "25%";
+  }
+}
