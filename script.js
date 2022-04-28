5bfce901972090eaad2a4bf79be343d3bb32b8e5
BuildTools <billyskaramanolis@outlook.com>
2022-04-28 17:07:20 +0300

    lol

--- script.js
+++ script.js
@@ -17,12 +17,13 @@ function downvote() {
 // Get the elements with class="column"
 var elements = document.getElementsByClassName("column");
 
-// Declare a "loop" variable
+// Declare a loop variable
 var i;
 
 // Full-width images
 function one() {
-  for (i = 0; i < elements.length; i++) {
+    for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "100%";  // IE10
     elements[i].style.flex = "100%";
   }
 }
@@ -30,6 +31,7 @@ function one() {
 // Two images side by side
 function two() {
   for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "50%";  // IE10
     elements[i].style.flex = "50%";
   }
 }
@@ -37,6 +39,18 @@ function two() {
 // Four images side by side
 function four() {
   for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "25%";  // IE10
     elements[i].style.flex = "25%";
   }
+}
+
+// Add active class to the current button (highlight it)
+var header = document.getElementById("myHeader");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
 }
